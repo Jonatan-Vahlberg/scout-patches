f95edb0c14019f27852635a46b5cfe702951475a
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2023-10-19 08:33:58 +0200

    start up project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
+import {nextui} from "@nextui-org/react";
+/* 
+onst sweden = {
+    DEFAULT: '#003660',
+    light: '#305b7d',
+    dark: '#002b4d',
+  }
+  
+  const wosm = {
+    DEFAULT: '#622599',
+    light: '#7e3da5',
+    dark: '#4a1a7f',
+  }
+  
+  const waggs = {
+    DEFAULT: '#201e4d',
+    light: '#2c2a5f',
+    dark: '#16143a',
+  }
+
+*/
+
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -12,7 +37,42 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        wosm: {
+          DEFAULT: '#622599',
+          light: '#7e3da5',
+          dark: '#4a1a7f',
+        },
+        sweden: {
+          DEFAULT: '#04549c',
+          light: '#305b7d',
+          dark: '#002b4d',
+        },
+        waggs: {
+          DEFAULT: '#201e4d',
+          light: '#2c2a5f',
+          dark: '#16143a',
+        },
+      },
+      button: {
+        primary: {
+          DEFAULT: '#003660',
+          hover: '#305b7d',
+          active: '#002b4d',
+        },
+        secondary: {
+          DEFAULT: '#622599',
+          hover: '#7e3da5',
+          active: '#4a1a7f',
+        },
+        tertiary: {
+          DEFAULT: '#201e4d',
+          hover: '#2c2a5f',
+          active: '#16143a',
+        },
+      }
     },
   },
-  plugins: [],
+  darkMode: 'class', // or 'media'
+  plugins: [nextui()],
 }
