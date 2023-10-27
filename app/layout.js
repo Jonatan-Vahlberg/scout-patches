fdbe895d00c90f80546f501c61f686b057a885fc
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2023-10-28 01:28:37 +0200

    fix screen layout issue

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
         {/* <main className="relative flex min-h-screen mt-[82px] z-[1] flex-col items-center justify-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-sweden-light to-sweden-dark"> */}
         <main className="relative flex mt-[82px] max-h-[calc(100vh-82px)] overflow-hidden z-[1] flex-col items-center justify-center">
         <Providers>
-          <div className='max-h-[calc(100vh-82px)] overflow-y-auto'>
+          <div className='max-h-[calc(100vh-82px)] overflow-y-auto w-screen'>
             {children}
           </div>
         </Providers>
