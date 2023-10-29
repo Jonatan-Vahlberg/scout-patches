cfc825944971d77eb7f53cbe71f52f61c81cbd8c
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2023-10-29 22:07:37 +0100

    expose header to providers

--- app/layout.js
+++ app/layout.js
@@ -15,15 +15,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-white`}>
+        <Providers>
         <Header/>
         {/* <main className="relative flex min-h-screen mt-[82px] z-[1] flex-col items-center justify-center bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-sweden-light to-sweden-dark"> */}
         <main className="relative flex mt-[82px] max-h-[calc(100vh-82px)] overflow-hidden z-[1] flex-col items-center justify-center">
-        <Providers>
           <div className='max-h-[calc(100vh-82px)] overflow-y-auto w-screen'>
             {children}
           </div>
-        </Providers>
         </main>
+        </Providers>
       </body>
     </html>
   )
