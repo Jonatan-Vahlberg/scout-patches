db8a65498a963860cf1b6022635d3af86d0732e4
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2023-11-04 00:45:42 +0100

    add user patches component

---
+++ app/components/profile/UserPatches.js
@@ -0,0 +1,41 @@
+"use client";
+
+import { usePatches } from "@/context/PatchContext";
+import { useUser } from "@/context/UserContext";
+import { useEffect, useState } from "react";
+import PatchListItem from "../Patch/PatchListItem";
+
+const UserPatches = () => {
+    const user = useUser()
+    const patches = usePatches();
+
+    const [shownPatches, setShownPatches] = useState([]);
+
+    useEffect(() => {
+        if(user.userPatches.length > 0 && patches.patches.length > 0) {
+            const _shownPatches = user.userPatches.map((userPatch) => {
+                let patch = patches.patches.find((patch) => patch.id === userPatch.id);
+                console.log("patch: ", patch)
+                return patch;
+            }).filter((patch) => patch !== undefined);
+            setShownPatches(_shownPatches);
+        }
+    }
+    ,[user.userPatches, patches.patches])
+    console.log("shownPatches: ", shownPatches, patches.patches ,user.userPatches.length)
+    return( 
+        <div className="flex flex-col flex-1 overflow-hidden">
+            <h2 className="text-2xl font-semibold my-4">Dina Märken</h2>
+            <div className="overflow-y-auto flex flex-col flex-1 gap-4 p-4">
+                {shownPatches.map((patch) => (
+                    <PatchListItem 
+                        key={patch.id}
+                        patch={patch}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default UserPatches;
